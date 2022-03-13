952fdbb988bde67ce4b044c87034bcaba9da8033
SeungHan <jk05018@naver.com>
2022-03-13 11:10:08 +0900

    effective java start

---
+++ effective_java/src/test/java/me/develop_han/effective_java/EffectiveJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.develop_han.effective_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
