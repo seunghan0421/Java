0aa306088e8e2d8230922d73a010fd9d2c63fe6e
SeungHan <jk05018@naver.com>
2022-03-21 12:05:22 +0900

    modern java in action setting

---
+++ modern_java_in_action/src/test/java/me/develop_han/modern_java_in_action/ModernJavaInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package me.develop_han.modern_java_in_action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernJavaInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
