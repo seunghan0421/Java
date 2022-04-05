80662463a84f2dd94244f60f73b2645a6b75dee7
SeungHan <jk05018@naver.com>
2022-04-05 09:35:21 +0900

    chapter 6 실습 완료

--- modern_java_in_action/src/main/java/me/develop_han/modern_java_in_action/chapter5/practice/Trader.java
+++ modern_java_in_action/src/main/java/me/develop_han/modern_java_in_action/chapter5/practice/Trader.java
@@ -1,2 +1,27 @@
-package me.develop_han.modern_java_in_action.chapter5.practice;public class Trader {
+package me.develop_han.modern_java_in_action.chapter5.practice;
+
+public class Trader {
+	private final String name;
+	private final String city;
+
+	public Trader(String name, String city) {
+		this.name = name;
+		this.city = city;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	@Override
+	public String toString() {
+		return "Trader{" +
+			"name='" + name + '\'' +
+			", city='" + city + '\'' +
+			'}';
+	}
 }
