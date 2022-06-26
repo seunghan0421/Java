e69206d9b14d702e5db329751edc451a2548bc5d
SeungHan <jk05018@naver.com>
2022-06-26 22:51:47 +0900

    build : optimistic lock project start:

---
+++ optimistic-lock/src/main/java/com/example/optimistic/lock/OptimisticLockApplication.java
@@ -0,0 +1,13 @@
+package com.example.optimistic.lock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimisticLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimisticLockApplication.class, args);
+	}
+
+}
---
+++ optimistic-lock/src/test/java/com/example/optimistic/lock/OptimisticLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.optimistic.lock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimisticLockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
