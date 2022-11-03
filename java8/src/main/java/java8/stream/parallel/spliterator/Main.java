cb1e4b55b46e1d85372af5258311bd9ec4063c2a
SeungHan <jk05018@naver.com>
2022-11-03 14:19:58 +0900

    Stream - using Spliterator - WordCounterSpliterator

---
+++ java8/src/main/java/java8/stream/parallel/spliterator/Main.java
@@ -0,0 +1,14 @@
+package java8.stream.parallel.spliterator;
+
+import java.util.stream.Stream;
+
+public class Main {
+
+	public int countWords(Stream<Character> stream) {
+		return stream.reduce(
+			new WordCounter(0, true),
+			WordCounter::accumulate,
+			WordCounter::combine)
+			.getCounter();
+	}
+}
---
+++ java8/src/main/java/java8/stream/parallel/spliterator/WordCounter.java
@@ -0,0 +1,27 @@
+package java8.stream.parallel.spliterator;
+
+public class WordCounter {
+	private final int counter;
+	private final boolean lastSpace;
+
+	public WordCounter(final int counter, final boolean lastSpace) {
+		this.counter = counter;
+		this.lastSpace = lastSpace;
+	}
+
+	public WordCounter accumulate(Character c){
+		if(Character.isWhitespace(c)){
+			return lastSpace ? this : new WordCounter(counter, true);
+		} else{
+			return lastSpace ? new WordCounter(counter + 1, false) : this;
+		}
+	}
+
+	public WordCounter combine(WordCounter wordCounter){
+		return new WordCounter(counter + wordCounter.counter, wordCounter.lastSpace);
+	}
+
+	public int getCounter() {
+		return counter;
+	}
+}
